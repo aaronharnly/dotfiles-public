bcc4db457d06de313653701419cd0be7a870980a
Aaron Harnly <git@bulk.harnly.net>
2008-12-29 14:27:12 -0500

    Initial checkin

---
+++ pull-from-cvs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ".bash-functions.sh"
+
+#do_echo 'cp src/analytics/bursts/bin/algorithm_pull.sh .'
+
---
+++ push-to-cvs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ".bash-functions.sh"
+
+#do_echo 'cp algorithm_run.rb src/analytics/bursts/bin/'
+
